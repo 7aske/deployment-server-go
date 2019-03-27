a9e86a2218a38a6ffb8f732fab605c315cfa0c4f
Nikola Tasic <ntasic7@gmail.com>
2019-03-27 01:39:57 +0100

    basic client commands working

--- client/dist/scripts/main.js
+++ client/dist/scripts/main.js
@@ -1,33 +1,91 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function dateTemplate(dateString) {
-    console.log(dateString);
-    var date = new Date(dateString);
-    return date.toDateString();
-    // return ``;
-}
-function appTemplate(app, running) {
-    return " <div class=\"card\">\n            <div class=\"card-header\" id=\"heading" + app.id + "\">\n\t\t\t\t<span class=\"float-right " + (running ? "online text-success" : "offline text-danger") + "\">" + (running ? "Online &bull;" : "Offline") + "</span>\n                <h2 class=\"mb-0\" style=\"cursor: pointer;\" data-toggle=\"collapse\" data-target=\"#collapse" + app.id + "\" aria-expanded=\"false\" aria-controls=\"collapse" + app.id + "\">\n\t\t\t\t\t" + app.name + "\n                </h2>\n                <h5 class=\"mb-0 text-muted\">\n\t\t\t\t\t" + app.repo + "\n\t\t\t\t</h5>\n            </div>\n            <div id=\"collapse" + app.id + "\" class=\"collapse\" aria-labelledby=\"heading" + app.id + "\" data-parent=\"#appContainer\">\n                <div class=\"card-body row\">\n                    <ul class=\"list-group list-group-flush col-lg-6 col-md-12\">\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>ID:</span><span>" + app.id + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Name:</span><span>" + app.name + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Repo:</span><span>" + app.repo + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Root:</span><span>" + app.name + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Port:</span><span>" + app.port + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Hostname:</span><span>" + app.hostname + "</span>\n                        </li>\n                    </ul>\n                    <ul class=\"list-group list-group-flush col-lg-6 col-md-12\">\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Deployed:</span><span>" + app.deployed + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>LastUpdated:</span><span>" + dateTemplate(app.last_updated) + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>LastRun:</span><span>" + app.last_run + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Uptime:</span><span>" + (running ? app.uptime : "offline") + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Runner:</span><span>" + app.runner + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Pid:</span><span>" + app.pid + "</span>\n                        </li>\n                    </ul>\n                </div>\n            </div>\n        </div>";
-}
-;
-var url = new URL(window.location.href);
+// interface FindResponse {
+// 	running: App[];
+// 	deployed: App[];
+// }
+var baseUrl = new URL(window.location.protocol + "//" + window.location.hostname + ":" + window.location.port);
 var token = document.cookie.split("; ").filter(function (e) { return e.startsWith("Authorization"); })[0].split("Bearer ")[1].replace("\"", " ");
 // @ts-ignore
 var tokenData = jwt_decode(token);
-console.log(tokenData);
-fetch(url.href + "/api/find").then(function (j) {
-    j.json().then(function (res) {
-        console.log(res);
-        var appsD = res.deployed;
-        var apps = res.running != null ? res.running : [];
-        appsD.forEach(function (a) {
-            var id = a.id;
-            if (apps.filter(function (app) { return id == app.id; }).length == 1) {
-                document.querySelector("#appContainer").innerHTML += appTemplate(apps.find(function (app) { return id == app.id; }), true);
-            }
-            else {
-                document.querySelector("#appContainer").innerHTML += appTemplate(a, false);
-            }
-        });
+function init() {
+    var url = baseUrl;
+    url.pathname = "/api/find";
+    fetch(url.href).then(function (j) {
+        j.json().then(function (res) {
+            console.log(res);
+            var appsD = res.deployed != null ? res.deployed : [];
+            var apps = res.running != null ? res.running : [];
+            appsD.forEach(function (a) {
+                if (apps.filter(function (app) { return a.id == app.id; }).length == 1) {
+                    document.querySelector("#appContainer").innerHTML += appTemplate(apps.find(function (app) { return a.id == app.id; }), true);
+                }
+                else {
+                    document.querySelector("#appContainer").innerHTML += appTemplate(a, false);
+                }
+            });
+        }).catch(function (err) { return console.log(err); });
     }).catch(function (err) { return console.log(err); });
-}).catch(function (err) { return console.log(err); });
+}
+function getButton(id, action) {
+    var icon, color, text;
+    switch (action) {
+        case "run":
+            color = "success";
+            icon = "running";
+            text = "Run";
+            break;
+        case "kill":
+            color = "warning";
+            icon = "skull";
+            text = "Kill";
+            break;
+        case "update":
+            color = "info";
+            icon = "sync";
+            text = "Update";
+            break;
+        case "remove":
+            color = "danger";
+            icon = "trash";
+            text = "Remove";
+            break;
+    }
+    return "<button class=\"btn btn-" + color + "\" data-action=\"" + action + "\" data-id=\"" + id + "\" onclick=\"doAction(event)\"><i class=\"fas fa-" + icon + " fa-2x\"></i><br>" + text + "</button>";
+}
+function dateTemplate(dateString) {
+    return new Date(dateString).toLocaleString();
+}
+function runnerIcon(runner) {
+    var r = "";
+    switch (runner) {
+        case "node":
+            r = "node";
+            break;
+        case "web":
+            r = "html5";
+            break;
+    }
+    return "<i class=\"fab fa-" + r + " fa-2x\"></i>";
+}
+function appTemplate(app, running) {
+    return " <div class=\"card\">\n            <div class=\"card-header\" id=\"heading" + app.id + "\">\n\t\t\t\t<span class=\"float-right " + (running ? "online text-success" : "offline text-danger") + "\">" + (running ? "Online <i class=\"fas fa-globe\"></i>" : "Offline <i class=\"fas fa-times-circle\"></i>") + "</span>\n                <h3 class=\"mb-2\" style=\"cursor: pointer;\" data-toggle=\"collapse\" data-target=\"#collapse" + app.id + "\" aria-expanded=\"false\" aria-controls=\"collapse" + app.id + "\">\n\t\t\t\t\t" + app.name + "\n                </h3>\n                <h6 class=\"mb-0 text-muted\">\n\t\t\t\t\t" + app.repo + "\n\t\t\t\t</h6>\n            </div>\n            <div id=\"collapse" + app.id + "\" class=\"collapse\" aria-labelledby=\"heading" + app.id + "\" data-parent=\"#appContainer\">\n                <div class=\"card-body row p-0\">\n                    <ul class=\"list-group list-group-flush col-lg-6 col-md-12 pl-1 pr-1\">\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>ID:</span><span>" + app.id + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Name:</span><span>" + app.name + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Repo:</span><span>" + app.repo + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Root:</span><span>" + app.name + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Port:</span><span>" + (app.port == 0 ? "none" : app.port) + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Hostname:</span><span>" + app.hostname + "</span>\n                        </li>\n                    </ul>\n                    <ul class=\"list-group list-group-flush col-lg-6 col-md-12 pl-1 pr-1\">\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Deployed:</span><span>" + dateTemplate(app.deployed) + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>LastUpdated:</span><span>" + dateTemplate(app.last_updated) + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>LastRun:</span><span>" + dateTemplate(app.last_run) + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Uptime:</span><span>" + (running ? app.uptime.replace(/\.(.*?)s/g, "s") : "offline") + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Runner:</span><span>" + runnerIcon(app.runner) + "</span>\n                        </li>\n                        <li class=\"list-group-item d-flex justify-content-between\">\n                            <span>Pid:</span><span>" + (app.pid == 0 ? "offline" : app.pid) + "</span>\n                        </li>\n                    </ul>\n                </div>\n                <div class=\"card-footer text-right\">\n                \t" + (running ? getButton(app.id, "kill") : getButton(app.id, "run")) + "\n    \t\t\t\t" + getButton(app.id, "update") + "\n    \t\t\t\t" + getButton(app.id, "remove") + "\n\t\t\t\t</div>\n            </div>\n        </div>";
+}
+function doAction(event) {
+    var url = baseUrl;
+    var btn = event.target;
+    var action = btn.attributes.getNamedItem("data-action").value;
+    var id = btn.attributes.getNamedItem("data-id").value;
+    var data = { app: id };
+    url.pathname = "/api/" + action;
+    fetch(url.href, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then(function (res) { return res.status == 200 ? location.reload() : null; })
+        .catch(function (err) { return console.log(err); });
+}
+init();
--- client/src/scripts/main.ts
+++ client/src/scripts/main.ts
@@ -1,4 +1,4 @@
-export interface App {
+interface App {
 	id: string;
 	repo: string;
 	name: string;
@@ -8,36 +8,100 @@ export interface App {
 	deployed: string;
 	last_updated: string;
 	last_run: string;
-	uptime: number;
+	uptime: string;
 	runner: string;
 	pid?: number;
 }
 
-export interface FindResponse {
-	running: App[];
-	deployed: App[];
+type ButtonActions = "run" | "kill" | "update" | "remove";
+// interface FindResponse {
+// 	running: App[];
+// 	deployed: App[];
+// }
+const baseUrl = new URL(window.location.protocol + "//" + window.location.hostname + ":" + window.location.port);
+const token = document.cookie.split("; ").filter(e => e.startsWith("Authorization"))[0].split("Bearer ")[1].replace("\"", " ");
+// @ts-ignore
+let tokenData = jwt_decode(token);
+
+function init() {
+	let url = baseUrl;
+	url.pathname = "/api/find";
+	fetch(url.href).then(j => {
+		j.json().then(res => {
+			console.log(res);
+			const appsD: App[] = res.deployed != null ? res.deployed : [];
+			const apps: App[] = res.running != null ? res.running : [];
+			appsD.forEach(a => {
+				if (apps.filter(app => a.id == app.id).length == 1) {
+					document.querySelector("#appContainer").innerHTML += appTemplate(apps.find(app => a.id == app.id), true);
+				} else {
+					document.querySelector("#appContainer").innerHTML += appTemplate(a, false);
+				}
+			});
+		}).catch(err => console.log(err));
+	}).catch(err => console.log(err));
 }
+
+function getButton(id: string, action: ButtonActions): string {
+	let icon, color, text;
+	switch (action) {
+		case "run":
+			color = "success";
+			icon = "running";
+			text = "Run";
+			break;
+		case "kill":
+			color = "warning";
+			icon = "skull";
+			text = "Kill";
+			break;
+		case "update":
+			color = "info";
+			icon = "sync";
+			text = "Update";
+			break;
+		case "remove":
+			color = "danger";
+			icon = "trash";
+			text = "Remove";
+			break;
+	}
+
+	return `<button class="btn btn-${color}" data-action=\"${action}\" data-id="${id}" onclick=\"doAction(event)\"><i class="fas fa-${icon} fa-2x"></i><br>${text}</button>`;
+
+}
+
 function dateTemplate(dateString: string): string {
-	console.log(dateString);
-	const date = new Date(dateString);
-	return date.toDateString();
-	// return ``;
+	return new Date(dateString).toLocaleString();
+}
+
+function runnerIcon(runner: string): string {
+	let r = "";
+	switch (runner) {
+		case "node":
+			r = "node";
+			break;
+		case "web":
+			r = "html5";
+			break;
+	}
+	return `<i class="fab fa-${r} fa-2x"></i>`;
 }
 
 function appTemplate(app: App, running: boolean): string {
 	return ` <div class="card">
             <div class="card-header" id="heading${app.id}">
-				<span class="float-right ${running ? "online text-success" : "offline text-danger"}">${running ? "Online &bull;" : "Offline"}</span>
-                <h2 class="mb-0" style="cursor: pointer;" data-toggle="collapse" data-target="#collapse${app.id}" aria-expanded="false" aria-controls="collapse${app.id}">
+				<span class="float-right ${running ? "online text-success" : "offline text-danger"}">${running ? "Online <i class=\"fas fa-globe\"></i>" : "Offline <i class=\"fas fa-times-circle\"></i>"}</span>
+                <h3 class="mb-2" style="cursor: pointer;" data-toggle="collapse" data-target="#collapse${app.id}" aria-expanded="false" aria-controls="collapse${app.id}">
 					${app.name}
-                </h2>
-                <h5 class="mb-0 text-muted">
+                </h3>
+                <h6 class="mb-0 text-muted">
 					${app.repo}
-				</h5>
+				</h6>
             </div>
             <div id="collapse${app.id}" class="collapse" aria-labelledby="heading${app.id}" data-parent="#appContainer">
-                <div class="card-body row">
-                    <ul class="list-group list-group-flush col-lg-6 col-md-12">
+                <div class="card-body row p-0">
+                    <ul class="list-group list-group-flush col-lg-6 col-md-12 pl-1 pr-1">
                         <li class="list-group-item d-flex justify-content-between">
                             <span>ID:</span><span>${app.id}</span>
                         </li>
@@ -51,55 +115,59 @@ function appTemplate(app: App, running: boolean): string {
                             <span>Root:</span><span>${app.name}</span>
                         </li>
                         <li class="list-group-item d-flex justify-content-between">
-                            <span>Port:</span><span>${app.port}</span>
+                            <span>Port:</span><span>${app.port == 0 ? "none" : app.port}</span>
                         </li>
                         <li class="list-group-item d-flex justify-content-between">
                             <span>Hostname:</span><span>${app.hostname}</span>
                         </li>
                     </ul>
-                    <ul class="list-group list-group-flush col-lg-6 col-md-12">
+                    <ul class="list-group list-group-flush col-lg-6 col-md-12 pl-1 pr-1">
                         <li class="list-group-item d-flex justify-content-between">
-                            <span>Deployed:</span><span>${app.deployed}</span>
+                            <span>Deployed:</span><span>${dateTemplate(app.deployed)}</span>
                         </li>
                         <li class="list-group-item d-flex justify-content-between">
                             <span>LastUpdated:</span><span>${dateTemplate(app.last_updated)}</span>
                         </li>
                         <li class="list-group-item d-flex justify-content-between">
-                            <span>LastRun:</span><span>${app.last_run}</span>
+                            <span>LastRun:</span><span>${dateTemplate(app.last_run)}</span>
                         </li>
                         <li class="list-group-item d-flex justify-content-between">
-                            <span>Uptime:</span><span>${running ? app.uptime : "offline"}</span>
+                            <span>Uptime:</span><span>${running ? app.uptime.replace(/\.(.*?)s/g, "s") : "offline"}</span>
                         </li>
                         <li class="list-group-item d-flex justify-content-between">
-                            <span>Runner:</span><span>${app.runner}</span>
+                            <span>Runner:</span><span>${runnerIcon(app.runner)}</span>
                         </li>
                         <li class="list-group-item d-flex justify-content-between">
-                            <span>Pid:</span><span>${app.pid}</span>
+                            <span>Pid:</span><span>${app.pid == 0 ? "offline" : app.pid}</span>
                         </li>
                     </ul>
                 </div>
+                <div class="card-footer text-right">
+                	${running ? getButton(app.id, "kill") : getButton(app.id, "run")}
+    				${getButton(app.id, "update")}
+    				${getButton(app.id, "remove")}
+				</div>
             </div>
         </div>`;
-};
-const url = new URL(window.location.href);
-const token = document.cookie.split("; ").filter(e => e.startsWith("Authorization"))[0].split("Bearer ")[1].replace("\"", " ");
-// @ts-ignore
-let tokenData = jwt_decode(token);
-console.log(tokenData);
+}
 
-fetch(url.href + "/api/find").then(j => {
-	j.json().then(res => {
-		console.log(res);
-		const appsD: App[] = res.deployed;
-		const apps: App[] = res.running != null ? res.running : [];
-		appsD.forEach(a => {
-			const id = a.id;
-			if (apps.filter(app => id == app.id).length == 1) {
-				document.querySelector("#appContainer").innerHTML += appTemplate(apps.find(app => id == app.id), true);
-			} else {
-				document.querySelector("#appContainer").innerHTML += appTemplate(a, false);
-			}
-		});
-	}).catch(err => console.log(err));
-}).catch(err => console.log(err));
+function doAction(event: Event) {
+	let url = baseUrl;
+	const btn = event.target as HTMLButtonElement;
+	const action = btn.attributes.getNamedItem("data-action").value;
+	const id = btn.attributes.getNamedItem("data-id").value;
+	let data = {app: id};
+	url.pathname = `/api/${action}`;
+	fetch(url.href, {
+		method: "POST", // *GET, POST, PUT, DELETE, etc.
+		mode: "cors", // no-cors, cors, *same-origin
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data), // body data type must match "Content-Type" header
+	})
+		.then(res => res.status == 200 ? location.reload() : null)
+		.catch(err => console.log(err));
+}
 
+init();
