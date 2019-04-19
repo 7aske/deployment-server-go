0a6df0eada4e0b175f2579cabf5185995b21f54d
Nikola Tasic <ntasic7@gmail.com>
2019-04-19 02:18:26 +0200

    scripts update(static link), gitignore rootfs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-go build -o bin/deployment-server cmd/deployment-server/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -ldflags '-w -extldflags "-static"' -o bin/deployment-server cmd/deployment-server/main.go
 echo "Compiled deployment-server"
 npm -C ./client install
 echo "Installed client modules"
